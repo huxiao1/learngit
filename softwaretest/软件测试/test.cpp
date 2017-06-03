2bcc4471208f804cef56b08f2db7d82c8a75aca0
Huxiao <603316223@qq.com>
2017-06-03 15:53:09 +0800

    wrote a readme file

---
+++ softwaretest/软件测试/test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float A,B,X,Y;
+	cout<<"请输入A,B,X:";
+	cin>>A;
+	cin>>B;
+	cin>>X;
+	cout<<"请输入你的期望值:";
+	cin>>Y;
+	if(A<=1 && B!=0)
+		X=X/A;
+	if((A==2)||(X>1))
+		X=X+1;
+	cout<<"输出的X值为:";
+	cout<<X<<endl;
+	return 0;
+}
